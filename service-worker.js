9476a409f66511160d82cac15a89d6dfc8b6fed0
dawang-dawang <1149504465@qq.com>
2026-09-17 10:53:29 +0800

    deploy v20260917ee: service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,17 +4,18 @@
    - 其余同源静态资源缓存优先、miss 时下载并缓存 → 日常打开秒开
    版本号：改 app.js/styles.css 等资源时，把 CACHE 名 bump 一次即可（旧缓存自动清理）
 */
-const CACHE = "lifewb-20260917ed";
+const CACHE = "lifewb-20260917ee";
 const PRECACHE = [
   "./",
   "./index.html",
-  "./styles.css?v=20260917ed",
-  "./app.js?v=20260917ed",
-  "./foods_base.js?v=20260917ed",
+  "./styles.css?v=20260917ee",
+  "./app.js?v=20260917ee",
+  "./foods_base.js?v=20260917ee",
   "./vue.global.prod.js",
   "./lunar.js",
   "./plantlib.js",
   "./manifest.webmanifest?v=20260811cw",
+  "./icons/工作平台.svg?v=20260917ee",
 ];
 
 self.addEventListener("install", (e) => {
